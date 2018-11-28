fc4517b35b7d283d991ba100a646144006fb2bcc
TJourde <null>
2018-11-28 16:27:43 +0100

    First commit from Geicar

---
+++ nucleo/Inc/steering.h
@@ -0,0 +1,18 @@
+#ifndef _STERRING_H_
+#define _STERRING_H_
+
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_gpio.h"
+
+
+/**
+* Set the speed of the steering motor. Speed value has to be between 0 and 100
+**/
+void steering_set_speed(GPIO_PinState en_steering, int speed);
+
+/**
+* Return the steering angle.
+**/
+int get_steering_angle(void);
+
+#endif
