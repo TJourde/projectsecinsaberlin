8e0314fe2f65d92325096ff0ea84131d048c6c78
VincentChtln <44566604+VincentChtln@users.noreply.github.com>
2019-01-08 18:48:00 +0100

    Update slave

--- Test_Raspberry_2/RPi_slave/com_slave.py
+++ Test_Raspberry_2/RPi_slave/com_slave.py
@@ -18,7 +18,19 @@ OM2 = 0x102
 TCP_PORT = 9000
 BUFFER_SIZE = 20
 
+global IpPink
+global IpBlack
+global Connection_ON
+global conn
 
+# Search own IP address to know which network it's on
+IpPink = os.popen('hostname -I').read() #get chain with '[@IP] \n'
+IpPink = IpPink[:len(IpPink)-2] #(suppress ' \n')
+if IpPink == '10.105.0.53': # IOT network
+    IpBlack = '10.105.0.55'
+elif IpPink == '192.168.137.12': # Berlin network
+    IpBlack = '192.168.137.201'
+    
 # *********************************************************
 # THREAD 1 - Connection à la voiture noire
 # *********************************************************
--- Test_Raspberry_2/RPi_slave/main_slave.py
+++ Test_Raspberry_2/RPi_slave/main_slave.py
@@ -5,20 +5,8 @@ import os
 import can
 import socket
 
-from com_slave import *
+import com_slave
 
-global IpPink
-global IpBlack
-global Connection_ON
-global conn
-
-# Search own IP address to know which network it's on
-IpPink = os.popen('hostname -I').read() #get chain with '[@IP] \n'
-IpPink = IpPink[:len(IpPink)-2] #(suppress ' \n')
-if IpPink == '10.105.0.53': # IOT network
-    IpBlack = '10.105.0.55'
-elif IpPink == '192.168.137.12': # Berlin network
-    IpBlack = '192.168.137.201'
 
 # Try connection to bus interface
 try:
