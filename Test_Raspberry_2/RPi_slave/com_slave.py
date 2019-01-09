b4d4a8ac7dbc0da4d00125d34d332e8d034d3c89
VincentChtln <44566604+VincentChtln@users.noreply.github.com>
2019-01-09 11:19:54 +0100

    Modif addr IP

--- Test_Raspberry_2/RPi_slave/com_slave.py
+++ Test_Raspberry_2/RPi_slave/com_slave.py
@@ -31,7 +31,7 @@ IpPink = IpPink[:len(IpPink)-2] #(suppress ' \n')
 if IpPink == '10.105.0.53': # IOT network
     IpBlack = '10.105.0.55'
 elif IpPink == '192.168.137.12': # Berlin network
-    IpBlack = '192.168.137.201'
+    IpBlack = '192.168.137.27'
 
 # *********************************************************
 # THREAD 1 - Connection à la voiture noire
