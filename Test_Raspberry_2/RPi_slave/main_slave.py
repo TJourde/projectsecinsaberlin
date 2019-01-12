6dabef385a0594f379ac8eddd76e45eb4aa80401
VincentChtln <44566604+VincentChtln@users.noreply.github.com>
2019-01-12 18:43:56 +0100

    Update main_slave.py

--- Test_Raspberry_2/RPi_slave/main_slave.py
+++ Test_Raspberry_2/RPi_slave/main_slave.py
@@ -54,14 +54,14 @@ while not VBS.Connection_ON.is_set():
             print('Socket error while receiving connection')
 
     # Check si l'adresse connectée est bien celle de la voiture noire, si oui commence l'envoi des données
-    elif (self.addr == self.IpBlack):
-        print('Connected to Berlin car with address' + repr(self.addr))
+    elif (addr == IpBlack):
+        print('Connected to Berlin car with address' + repr(addr))
         Connection_ON.set()
 
     # Si quelqu'un autre que la RPi noire se connecte, le déclare, clôt la connection et se met en attente d'une nouvelle
-    elif (self.addr != self.IpBlack):
+    elif (addr != IpBlack):
         Connection_ON.clear()
-        print('Connected to unknown device, with address ' + repr(self.addr))
+        print('Connected to unknown device, with address ' + repr(addr))
         print('Closing communication channel')
         VBS.conn.close()
         addr = -1
